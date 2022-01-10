27e471bf948ec5338d00f55a42e74e8ee1a85c76
Aaron Pavlick <apavlick@yext.com>
2022-01-10 11:15:07 -0500

    switchin to solstice answers experience

--- src/config/answersHeadlessConfig.ts
+++ src/config/answersHeadlessConfig.ts
@@ -4,7 +4,7 @@ type HeadlessProviderProps = Parameters<typeof AnswersHeadlessProvider>[0];
 
 export const answersHeadlessConfig: HeadlessProviderProps = {
   apiKey: '50706f70f096a4e7906985a583210869',
-  experienceKey: 'hackathon',
+  experienceKey: 'solstice',
   locale: 'en',
   sessionTrackingEnabled: true
 };
