83a0878c5243e24dcefb87338631385c5b227077
Aaron Pavlick <apavlick@yext.com>
2022-01-12 17:28:31 -0500

    fake heading buttons

--- src/components/SolsticeHeader.tsx
+++ src/components/SolsticeHeader.tsx
@@ -2,13 +2,13 @@
 export function SolsticeHeader(): JSX.Element {
   return (
     <div className="flex flex-row justify-between px-4 h-24 items-center">
-      <div className="flex space-x-32">
+      <div className="flex items-center space-x-32">
         <div className="font-heading font-black text-lg">Solstice</div>
-        {/* <div className="flex font-heading">
-          <div>LOCATIONS</div>
-          <div>CLASSES</div>
-          <div>TRAINERS</div>
-        </div> */}
+        <div className="flex font-heading font-bold  space-x-16">
+          <div>ABOUT US</div>
+          <div>OUR COMMITMENT TO CLEAN</div>
+          <div>BLOG</div>
+        </div>
       </div>
       <div className="flex space-x-8 items-center font-heading font-bold" >
         <div className="align-middle">LOG IN</div>
