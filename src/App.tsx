e0fbecca13b5f9a168a9fb27dcd0bb3983e53470
Aaron Pavlick <apavlick@yext.com>
2022-01-24 18:07:32 -0500

    mobile layout for header

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,43 @@ import StandardLayout from './pages/StandardLayout';
 import { AnswersHeadlessProvider } from '@yext/answers-headless-react';
 import { answersHeadlessConfig } from './config/answersHeadlessConfig';
 import { routeConfig } from './config/routeConfig';
+import { useEffect, useState, createContext } from 'react';
+
+export const ResponsiveContext = createContext(false);
 
 export default function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => { 
+
+    updateDimensions();
+
+    window.addEventListener('resize', updateDimensions);
+    return () => 
+      window.removeEventListener('resize', updateDimensions);
+  }, []);
+
+  const updateDimensions = () => {
+    const width = window.innerWidth;
+    setIsMobile(width < 1024);
+  }
+
   return (
-    <div 
+    <ResponsiveContext.Provider value={isMobile}>
+      <div 
       // TODO: need better way to do this
-      style={{  margin: 0, padding: 0, minHeight: '100vh', backgroundColor: 'black' }}>
-      <AnswersHeadlessProvider {...answersHeadlessConfig}>
-        <div className='flex justify-center px-8 py-6 bg-black text-white'  >
-          <div className='w-full max-w-7xl' >
-            <PageRouter
-              Layout={StandardLayout}
-              routes={routeConfig}
-            />
+        style={{  margin: 0, padding: 0, minHeight: '100vh', minWidth:'100vh', backgroundColor: 'black' }}>
+        <AnswersHeadlessProvider {...answersHeadlessConfig}>
+          <div className='flex justify-center px-8 py-6 bg-black text-white w-full'  >
+            <div className='w-full max-w-7xl' >
+              <PageRouter
+                Layout={StandardLayout}
+                routes={routeConfig}
+              />
+            </div>
           </div>
-        </div>
-      </AnswersHeadlessProvider>
-    </div>
+        </AnswersHeadlessProvider>
+      </div>
+    </ResponsiveContext.Provider>
   );
 }
--- src/components/SolsticeHeader.tsx
+++ src/components/SolsticeHeader.tsx
@@ -1,21 +1,73 @@
+import { useContext, useState } from "react";
+import { ResponsiveContext } from "../App";
+import { ReactComponent as HamburgerMenuIcon } from '../icons/hamburger_menu.svg';
+import { ReactComponent as CloseMenuIcon } from '../icons/close_menu.svg';
+
+const cssClasses = {
+  container: "flex px-4 h-24 items-center justify-between",
+  topContainer: "flex items-center",
+  solsticeLabel: "font-heading text-lg",
+  headerContainer: "flex  items-center font-heading",
+  headingsContainer: "flex space-x-16 font-heading",
+  membershipButtonsContainer: 'flex items-center space-x-4 float-right font-heading',
+  dropdownModal: 'fixed top-24 left-0 right-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full'
+}
+
+const options = ['ABOUT US', 'OUR COMMITMENT TO CLEAN', 'BLOG', 'SIGN IN', 'SIGN UP'];
 
 export function SolsticeHeader(): JSX.Element {
-  return (
-    <div className="flex flex-row justify-between px-4 h-24 items-center">
-      <div className="flex items-center space-x-32">
-        <div className="font-heading font-black text-lg">Solstice</div>
-        <div className="flex font-heading font-bold  space-x-16">
-          <div>ABOUT US</div>
-          <div>OUR COMMITMENT TO CLEAN</div>
-          <div>BLOG</div>
-        </div>
-      </div>
-      <div className="flex space-x-8 items-center font-heading font-bold" >
-        <div className="align-middle">LOG IN</div>
-        <div className="border rounded-md">
-          <div className="py-4 px-10">JOIN US</div>
-        </div>
+  const isMobile = useContext(ResponsiveContext);
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const renderHeadings = () => (
+    <div className={cssClasses.headingsContainer}>
+      <div>ABOUT US</div>
+      <div>OUR COMMITMENT TO CLEAN</div>
+      <div>BLOG</div>
+    </div>
+  );
+
+  const renderMembershipButtons = () => (
+    <div className={cssClasses.membershipButtonsContainer}>
+      <div className="align-middle">LOG IN</div>
+      <div className="border rounded-md">
+        <div className="py-4 px-10">JOIN US</div>
       </div>
     </div>
+  );
+
+  const renderDropdownMenuIcon = () => {
+    if(isDropdownOpen){
+      return (<CloseMenuIcon onClick={() => setIsDropdownOpen(false)}/> );
+    } else {
+      return (<HamburgerMenuIcon onClick={() => setIsDropdownOpen(true)}/>);
+    }
+  }
+
+  const renderDropdownModal = () => (
+    <div className="fixed top-24 left-0 right-0 bg-black h-full w-full z-10">
+      <div className="px-12 py-4" >
+        {options.map(option => renderDropdownOption(option))}
+      </div>
+    </div>
+  );
+
+  const renderDropdownOption = (option: string) => (
+    <div className="py-8 border-t-2">{option}</div>
+  );
+
+  return (
+    <div>
+      <div className={cssClasses.container}>
+        <div className={cssClasses.topContainer}>
+          <div className={cssClasses.solsticeLabel}>Solstice</div>
+        </div>
+        {isMobile && renderDropdownMenuIcon() }
+        {!isMobile && renderHeadings()}
+        {!isMobile && renderMembershipButtons()}
+      </div>
+      {isDropdownOpen && renderDropdownModal()}
+    </div>
   )
-}
+};
